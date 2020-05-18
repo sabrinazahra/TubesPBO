7aeb7451f16c25d2dd7d3e505ca9aac9b66d320d
DimSk21 <58171378+DimSky21@users.noreply.github.com>
2020-05-18 19:14:06 +0700

    Add files via upload

---
+++ updatetubesdahlah/JavaTubes/src/Config.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
+public class Config
+{
+
+    private static Connection MySQLConfig;
+    public static Connection configDB()throws SQLException
+    {
+        try
+        {
+            String url ="jdbc.mysql://localhost:3306/tubes";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("fail to connect " + e.getMessage());
+        }
+        return MySQLConfig;
+            
+    }
+}
